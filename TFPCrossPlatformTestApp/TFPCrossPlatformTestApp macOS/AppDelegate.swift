29ac369699ccfe2bb9571d5f20ef30d139923fec
Andrew Finke <andrewfinke@me.com>
2017-10-13 23:25:27 -0500

    Experiments

---
+++ TFPCrossPlatformTestApp/TFPCrossPlatformTestApp macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TFPCrossPlatformTestApp macOS
+//
+//  Created by Andrew Finke on 10/13/17.
+//  Copyright © 2017 Andrew Finke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
