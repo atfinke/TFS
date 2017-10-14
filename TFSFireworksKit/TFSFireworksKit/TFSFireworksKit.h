400479f8880da1445cd35969866f875ea41623b9
Andrew Finke <andrewfinke@me.com>
2017-10-13 20:24:39 -0500

    Init Project

---
+++ TFSFireworksKit/TFSFireworksKit/TFSFireworksKit.h
@@ -0,0 +1,19 @@
+//
+//  TFSFireworksKit.h
+//  TFSFireworksKit
+//
+//  Created by Andrew Finke on 10/13/17.
+//  Copyright © 2017 Andrew Finke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFSFireworksKit.
+FOUNDATION_EXPORT double TFSFireworksKitVersionNumber;
+
+//! Project version string for TFSFireworksKit.
+FOUNDATION_EXPORT const unsigned char TFSFireworksKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFSFireworksKit/PublicHeader.h>
+
+
